48aa0631347650f15b67e000b85612bdb77dde3f
Will Sheffler <willsheffler@gmail.com>
2025-04-13 17:25:54 -0700

    fix(ci): fix nonlocal build

--- evn/format/__init__.py
+++ evn/format/__init__.py
@@ -3,6 +3,7 @@ import sys
 import evn
 
 with evn.cd_project_root() as project_exists:
+    using_local_build = False
     if project_exists and os.path.exists('_build'):
         assert os.path.exists('pyproject.toml')
         os.system('doit build')
@@ -11,13 +12,11 @@ with evn.cd_project_root() as project_exists:
             print(f'_build/py3{sys.version_info.minor}')
             from _detect_formatted_blocks import *  # type: ignore
             from _token_column_format import *  # type: ignore
-
             using_local_build = True
         except ImportError:
-            using_local_build = False
+            pass
         finally:
-            sys.path.pop(
-                0)  # Remove the build path so it doesn't interfere with import
+            sys.path.pop(0)  # Remove the build path so it doesn't interfere with import
     if not using_local_build:
         from evn.format._detect_formatted_blocks import *  # type: ignore
         from evn.format._token_column_format import *  # type: ignore
